7d6912b7c25d3a8e547c6fe3ddb19cbe2b92ff2f
Chris Muller <chris.muller@outlook.com>
2022-07-21 14:58:00 -0600

    created home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
 import React from "react";
+import Home from './Home'; 
 import {
   BrowserRouter as Router,
   Routes,
@@ -54,27 +53,6 @@ export default function App() {
 
 // You can think of these components as "pages"
 // in your app.
-
-function Home() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> 
-    </div>
-  );
-}
 
 function About() {
   return (
